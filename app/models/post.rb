1abc046442af108c9ed75870240cc6d0913a0d78
Olessia Potapova <olessia.potapova@gmail.com>
2017-08-09 19:31:51 -0500

    navbar is transparent, carousel titles are white

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	belongs_to :user, optional: true
 	belongs_to :category
-
+	validates_presence_of :post_title, length: { minimum: 1 }
+	validates_presence_of :post_description, length: { minimum: 10 }
 
 end
