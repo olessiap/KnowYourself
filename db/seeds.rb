3917048d7a5f8adfd6306c4275e5846fd769dfee
Olessia Potapova <olessia.potapova@gmail.com>
2017-08-10 14:31:21 -0500

    first heroku go

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#posts = Post.create([{post_title: "post1", post_description: "lalallalalalla", user_id: "1", category_id: "1"}, {post_title: "post2", post_description: "lalallalalalla", user_id: "1", category_id: "1"}])
+
+
+#categories = Category.create([{category_title: "great sharks" }, {category_title: "Amazing Falafels"} ])
+
+
+Category.create!([{category_title: "Special Events"},
+		{category_title: "Around The World"},
+		{category_title: "Amazing Falafels"},
+		{category_title: "Relationships"},
+		{category_title: "family"}
+		])
+
+
+5.times do |i|
+	Post.create!(
+		post_title:Faker::Beer.name,
+		post_description:Faker::Hipster.paragraphs,
+		user_id: "1",
+		category_id: "1"
+		#image_file_name: File.open(Rails.root, "/public/seed/#{file_name}.jpg")
+		)
+end
